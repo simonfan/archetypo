9c5cc12fb5786404bb72499b9b757d8edfc2dd61
simonfan <simonxd@gmail.com>
2014-03-22 19:02:09 -0300

    asynchronous rendering

--- built/archetypo.full.js
+++ built/archetypo.full.js
@@ -48,12 +48,24 @@ define('__archetypo/registry/index',['require','exports','module','lodash','lowe
  * @submodule view
  */
 
-define('__archetypo/view/initialize/render',['require','exports','module','lodash'],function (require, exports, module) {
+define('__archetypo/view/initialize/render',['require','exports','module','lodash','q'],function (require, exports, module) {
 	
 
-	var _ = require('lodash');
+	var _ = require('lodash'),
+		q = require('q');
 
-	module.exports = function render(options) {
+	/**
+	 * Does all the dirty stuff.
+	 *
+	 * @method _render
+	 * @private
+	 *
+	 */
+	function _render(options) {
+
+		// create a deferred object
+		var defer = q.defer();
+
 		// [1] Templating and replacement
 		// If there is an 'html' property
 		// build up an element with it place it within $el.
@@ -61,12 +73,59 @@ define('__archetypo/view/initialize/render',['require','exports','module','lodas
 		// [1.1] retrieve AND normalize the element object
 		this.$el = _.isObject(options.$el) ? options.$el : options.el;
 
-		var html = options.html || this.html;
-		if (html) {
-			// [1.2] place
-			this.$el.html(html);
+		var data = this.$el.data();
+
+		// [2] retrieve html
+		if (data.html) {
+			// async resolution
+
+			// if a html data property is set on
+			// the html element,
+			// assume it is a module name.
+			require(['text!' + data.html], _.bind(function (html) {
+
+				this.$el.html(html);
+
+				defer.resolve();
+
+			}, this));
+
+		} else {
+
+			// immediate resolution
+			var html = options.html || this.html;
+
+			if (html) {
+				this.$el.html(html);
+
+				defer.resolve();
+			}
 		}
+
+		// return promise no matter what.
+		return defer.promise;
 	};
+
+
+
+	/**
+	 * Verifies for cached render call.
+	 *
+	 * @method render
+	 * @param options
+	 */
+	module.exports = function render(options) {
+
+		if (!this.renderPromise) {
+			// render was not called yet,
+			// so call it, set the promise and return
+
+			this.renderPromise = _render.apply(this, arguments);
+		}
+
+		// always return the promise.
+		return this.renderPromise;
+	}
 
 });
 
@@ -204,25 +263,44 @@ define('__archetypo/view/index',['require','exports','module','lodash','dockable
 
 		arguments[0] = options || {};
 
+		var args = Array.prototype.slice.call(arguments);
+
+		// save reference to app.
+		this.app = options.app;
+
 		// [1] Templating and replacement
 		// If there is an 'html' property
 		// build up an element with it place it within $el.
-		render.apply(this, arguments);
+		// render is ASYNC
+		this.render(options)
+			.then(_.bind(function () {
+
+				// [2] Invoke dockable-view.
+				//     MUST come afte render,
+				//     so that the docks take effect.
+				dockableView.prototype.initialize.apply(this, args);
+
+				// [3] Start subviews
+				subviews.apply(this, args);
+
+			}, this));
 
 
-		// [2] Invoke dockable-view.
-		//     MUST come afte render.
-		dockableView.prototype.initialize.apply(this, arguments);
-
-
-		// [3] Register view
-		register.apply(this, arguments);
-
-		// [4] Start subviews
-		subviews.apply(this, arguments);
+		// [4] Register view (synchornously)
+		register.apply(this, args);
 	});
 
 	archView.proto({
+
+		/**
+		 * Renders the view and returns promise.
+		 *
+		 * @method render
+		 *
+		 */
+		render: render,
+
+
 		/**
 		 * Selects the view objcets that descend from this view.
 		 *
